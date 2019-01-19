4984a65eb7e2532da9a3accf2595b9be2107e14d
Farouk <sfarouk298@gmail.com>
2019-01-19 02:20:46 +0100

    Backend deployed to Google app engine

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
 
